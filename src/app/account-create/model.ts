e772c1dfcf041d9b48e9554289b866ebdd8fcba6
Balcells Edouard <balcellsedouard1@gmail.com>
2018-12-11 17:32:10 +0100

    user create account 1/3

---
+++ src/app/account-create/model.ts
@@ -0,0 +1,10 @@
+export class FormInput {
+    name:string;
+    firstName:string;
+    adress:string;
+    phone:number;
+    email:string;
+    birthDate:number;
+    password:string;
+    passwordConfirm:string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthComponent } from './auth/auth.component';
 import {FormsModule} from "@angular/forms";
 import {StatutConnecteService} from "./auth/statut-connecte.service";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import { AccountCreateComponent } from './account-create/account-create.component';
 
 const routes: Routes = [
   { path:'tech', component: TechComponent, canActivate:[StatutConnecteService]}, // /tech accessible uniquement si connecté
@@ -21,7 +22,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     TechComponent,
-    AuthComponent
+    AuthComponent,
+    AccountCreateComponent
   ],
   imports: [
     BrowserModule,
