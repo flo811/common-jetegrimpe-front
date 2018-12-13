5dabb21b2105aacb0e1eb62b9134290f03a5224d
Balcells Edouard <balcellsedouard1@gmail.com>
2018-12-13 17:41:50 +0100

    ase enter the commit message for your changes. Lines starting  have 1 and 22 different commits each, respectively.

--- src/app/account-create/account-create.component.ts
+++ src/app/account-create/account-create.component.ts
@@ -1,33 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountCreateService } from '../services/account-create.service';
 
-export class FormInput {
-  name:string;
-  firstName:string;
-  adress:string;
-  phone:number;
-  email:string;
-  birthDate:number;
-  password:string;
-  passwordConfirm:string;
-}
-
 @Component({
   selector: 'app-account-create',
   templateUrl: './account-create.component.html',
   styleUrls: ['./account-create.component.scss']
 })
+
 export class AccountCreateComponent implements OnInit {
 
-  formInput:FormInput = new FormInput();
+  lastName: string;
+  firstName: string;
+  adress: string;
+  phone: number;
+  email: string;
+  birthDate: Date;
+  password: string;
+  passwordConfirm: string;
 
-  constructor(private acService:AccountCreateService) { }
+  constructor(private acService: AccountCreateService) { }
 
   submit() {
-    this.acService.sendInputs(this.formInput)
+    this.acService.sendInputs({
+      "lastName": this.lastName, "firstName": this.firstName, "adress": this.adress, "phone": this.phone, "email": this.email, "birthDate": this.birthDate, "password": this.password
+    })
   }
 
   ngOnInit() {
   }
-
 }
--- src/app/services/account-create.service.ts
+++ src/app/services/account-create.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { FormInput} from 'src/app/account-create/account-create.component';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AccountCreateService {
 
+  constructor(private _http: HttpClient) { }
 
-  constructor(private _http:HttpClient) { }
-
-
-  sendInputs(send:FormInput):Promise<any>{
-    return this._http.post(environment.baseUrl.concat("/user/creer"), send).toPromise()
+  sendInputs(datas: any): Promise<any> {
+    return this._http.post(environment.baseUrl.concat("/user/creer"), datas).toPromise()
   }
 }
