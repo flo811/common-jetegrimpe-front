998346a71273e4111bece35b0cb2b9b99cb8f039
chloe <chloekaczmarek0@gmail.com>
2018-12-19 17:42:47 +0100

    commit mercredi soir

--- src/app/graphicalElements/services/person.service.ts
+++ src/app/graphicalElements/services/person.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Collegue } from 'src/app/auth/auth.domains';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,15 @@ export class PersonService {
 
   constructor(private _http:HttpClient) { }
 
+
+  // Add new user
   addPerson(name:string, firstName:string, adress:string, phone:number, email:string, birthDate:string, password:string):Promise<any>{
     return this._http.post(environment.baseUrl + "utilisateur/creer-compte", {name, firstName, adress, phone, email, birthDate, password}).toPromise()
   }
+
+  // Patch method to modify user
+  modifyPerson(collegue:Collegue):Promise<Collegue>{
+    return <Promise<Collegue>>this._http.patch(environment.baseUrl + "utilisateur/profil", collegue).toPromise()
+  }
+
 }
